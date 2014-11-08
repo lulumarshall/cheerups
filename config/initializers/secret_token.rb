28454745c64115cb5261abf8ca1cd73bd379d15e
lulumarshall <lulumarshall@gmail.com>
2014-11-08 12:21:14 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheerups::Application.config.secret_token = '048dd14f8f38f780a7cab1b45c9113d6bf9bd23165f0669aeaccb64d41133db56156ce01f95f23a638b2e44935a66b4f13fcc8c37ef2d38d9ae26bc98c41966d'
