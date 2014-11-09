831e29b4883d4944f0d922750fc0457ad31e53e5
lulumarshall <lulumarshall@gmail.com>
2014-11-09 16:21:26 +0000

    added relationships controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < Devise::RegistrationsController
-
+  # before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  
   def index
     @users = User.all
   end
@@ -22,5 +23,18 @@ class UsersController < Devise::RegistrationsController
     # to confirm that we inherit from ApplicationController
     logger.info self.class.ancestors
     super
+  end
+  def following 
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following 
+    render 'show_follow' 
+  end 
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+    
   end
 end
