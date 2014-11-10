35ba2a4beafd8cab8385372bb92ea6a54bc2f3e6
lulumarshall <lulumarshall@gmail.com>
2014-11-10 15:47:50 +0000

    added gravatar using email address to find image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,13 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user, options = { size: 50 })
-    gravatar_id = Digest::MD5::hexdigest(user.username.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+  def gravatar_for(user)
+    # if user.avatar_url.present?
+    #   user.avatar_url
+    # else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}.png?s=48"
+      # image_tag(gravatar_url, alt: user.username, class: "gravatar")
+    # end
   end
 end
