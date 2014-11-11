2bba06272137fca77c8d380234546ccae9c62714
Benjamin Sunderland <Ben@sunderland.eu.com>
2014-11-11 09:34:12 +0000

    ransack

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed 
   has_many :followers, through: :passive_relationships, source: :follower 
 
+
+  
   # Follows a user.
     def follow(other_user)
       active_relationships.create(followed_id: other_user.id)
