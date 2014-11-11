c36c091af6cf2e090b363dc833db9b7967a4de9f
lulumarshall <lulumarshall@gmail.com>
2014-11-11 16:59:59 +0000

    change pre master merge

--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(:version => 20141111105635) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "friendships", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.string   "friend_status"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-  end
-
   create_table "promotes", :force => true do |t|
     t.integer  "user_id"
     t.integer  "cheer_id"
