0c903598425128084e79b1b89ae34bfa8104d727
lulumarshall <lulumarshall@gmail.com>
2014-11-12 15:16:56 +0000

    twitter feed added to the side bar on the side bar layout.  Not dynamic at the moment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :ransack
+  before_filter :twitter
   include SessionsHelper
   include UsersHelper
 
-def ransack
-	@q = User.search(params[:q])
-	@users = @q.result(distinct: true)
+  def ransack
+	 @q = User.search(params[:q])
+	 @users = @q.result(distinct: true)
 	end
+
+
+  def twitter
+    username = 'lulumarshall'
+    options = {:count => 10, :include_rts => true}
+    @tweets = $client.user_timeline(username, options)
+  end
 end
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR_CONSUMER_KEY"
-  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_ACCESS_SECRET"
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_ID']
+  config.consumer_secret     = ENV['TWITTER_SECRET']
+  config.access_token        = ENV['TWITTER_TOKEN']
+  config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
 end
