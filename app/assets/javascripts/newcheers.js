384aa4d0579cfb690c20a733128282166663fd61
lulumarshall <lulumarshall@gmail.com>
2014-11-13 14:58:24 +0000

    ajax added to the delete button

--- app/assets/javascripts/newcheers.js
+++ app/assets/javascripts/newcheers.js
@@ -8,11 +8,11 @@ function request(method, url, data){
 }
 
 //add to the Cheer list 
-function appendNewCheer(data){
+//function appendNewCheer(data){
   
-  $('<li id="cheer-'+ data.id + '">' + '<span class="user"><a href="/users/'+ data.user_id + '"></a></span><span class="content">'+ data.cheerup +'</span><span class="timestamp"></span><div class="edit"><a href="/cheers/'+ data.id +'/edit">Edit</a></div><div class="destroy"><a href="/cheers/'+ data.id + '" data-confirm="Are you sure?" data-method="delete" rel="nofollow">x</a></div><span class="/cheers/'+ data.id +'/cheerup" data-method="put" rel="nofollow">cheerUp!</a></span><span class="score">0</span></li>').prependTo("#cheers") 
+  //$('<li id="cheer-'+ data.id + '">' + '<span class="user"><a href="/users/'+ data.user_id + '"></a></span><span class="content">'+ data.cheerup +'</span><span class="timestamp"></span><div class="edit"><a href="/cheers/'+ data.id +'/edit">Edit</a></div><div class="destroy"><a href="/cheers/'+ data.id + '" data-confirm="Are you sure?" data-method="delete" rel="nofollow">x</a></div><span class="/cheers/'+ data.id +'/cheerup" data-method="put" rel="nofollow">cheerUp!</a></span><span class="score">0</span></li>').prependTo("#cheers") 
  
-}  
+  
 
 //POST Cheer 
 function createCheer(){
@@ -28,7 +28,33 @@ function createCheer(){
   })
 }
 
+function deleteCheer(event){
+  event.preventDefault();
+  var $this = $(this);
+  console.log($this)
+  var cheerId = $this.data("id");
+  console.log(cheerId)
+  $.ajax({
+    url: '/cheers/' + cheerId,
+    type: 'DELETE',
+    dataType: 'json'
+  }).success(function(){
+   $this.closest('li').remove();
+  })
+}
 
+// railsAjax.deleteTask = function(){
+//   $this = $(this);
+//   var taskId = $this.data('id')
+//       // console.log('delete post')
+//       $.ajax({
+//         url: '/tasks/' + taskId,
+//         type: 'DELETE',
+//         dataType: 'json'
+//       }).success(function(data){
+//         console.log(data, 'the ajax call was successful');
+//         $this.closest('li').remove();
+//       });
 
 
 $(document).ready(function(){
@@ -37,4 +63,11 @@ $(document).ready(function(){
     if(event.which == '13')createCheer()
       console.log('key press')
   });
+
+  $('#cheers').on('click', ".destroy", deleteCheer);
+
 });
+
+
+
+
