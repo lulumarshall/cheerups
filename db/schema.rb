6318e13a76bdbe39315983e3eff93dedb5188500
lulumarshall <lulumarshall@gmail.com>
2014-11-13 15:16:26 +0000

    fixed illegal nesting on the application layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20141113110513) do
 
   create_table "cheers", :force => true do |t|
@@ -19,14 +18,6 @@ ActiveRecord::Schema.define(:version => 20141113110513) do
     t.string   "cheerup"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "friendships", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.string   "friend_status"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
   end
 
   create_table "promotes", :force => true do |t|
