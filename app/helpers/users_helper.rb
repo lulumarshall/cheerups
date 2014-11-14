0f80da8753248cd630930582873a427a7419345c
lulumarshall <lulumarshall@gmail.com>
2014-11-14 10:21:58 +0000

    fixed bugs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,7 @@ module UsersHelper
   def gravatar_for(user)
     if user.image.present?
       user.image
+
     elsif 
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
       gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}.png?s=48"
