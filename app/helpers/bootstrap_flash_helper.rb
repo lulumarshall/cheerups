6a922251a811ca6985f2a44c7246127fa6f7eacf
lulumarshall <lulumarshall@gmail.com>
2014-11-14 10:56:56 +0000

    removed the flash messages

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -13,12 +13,12 @@ module BootstrapFlashHelper
       type = :danger  if type == :error
       next unless ALERT_TYPES.include?(type)
 
-      Array(message).each do |msg|
-        text = content_tag(:div,
-                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-                           msg, :class => "alert fade in alert-#{type} #{options[:class]}")
-        flash_messages << text if msg
-      end
+      # Array(message).each do |msg|
+      #   text = content_tag(:div,
+      #                      content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+      #                      msg, :class => "alert fade in alert-#{type} #{options[:class]}")
+      #   flash_messages << text if msg
+      # end
     end
     flash_messages.join("\n").html_safe
   end
